f6d11a01d1f6e3b295df88fbad490ea61acd1bc9
up1 <somkiat.p@gmail.com>
2020-05-25 01:56:28 +0700

    Demo code of MVC

---
+++ demo-mvc/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using demo_mvc.Models;
+
+namespace demo_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly TestService service;
+        private readonly ILogger<HomeController> logger;
+        public HomeController(TestService service, ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+            this.service = service;
+
+        }
+
+        public IActionResult Index()
+        {
+            service.Test();
+            return View();
+        }
+
+        public IActionResult Miss()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ demo-mvc/TestService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace demo_mvc
+{
+    public class TestService
+    {
+        private readonly ILogger<TestService> logger;
+
+        public TestService(ILogger<TestService> logger)
+        {
+            this.logger = logger;
+
+        }
+
+        public void Test()
+        {
+            logger.LogWarning("I'm writing a message");
+        }
+    }
+}
